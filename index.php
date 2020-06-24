26677a3421504b452a7b74af1241a23fa14fee06
tharindu97 <tharindukavishna@gmail.com>
2020-06-24 10:28:23 +0530

    login_ui

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Book Shop</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <style>
+        body{
+            background-image:url(assest/img/bookshop.jpg);
+        }
+    </style>
+</head>
+<body>
+    
+    <div class="container"><!-- Container -->
+        <div class="row"><!-- Row 1 -->
+            <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+                <div class="card my-5"><!-- Card -->
+                    <div class="card-body"><!-- Card Body -->
+                        <h5 class="text-center my-2">:#: Book Shop :#:</h5>
+                        <form action="#" class="form" role="form" method="post">
+                            
+                            <div class="my-3"><!-- txtusername -->
+                                <input type="text" class="form-control" id="txtUserName" name="txtUserName" placeholder="User Name">
+                            </div><!-- End of txtusername -->
+
+                            <div class="my-3"><!-- password -->
+                                <input type="password" class="form-control" id="txtPassword" name="txtPassword" placeholder="Password">
+                            </div><!-- End of password -->
+
+                            <div class="my-4"><!-- button -->
+                                <button class="btn btn-danger btn-block text-uppercase">Login</button>
+                            </div><!-- End of button -->
+
+                            <hr class="my-4">
+                            <h6>Create Your Account <a href="./view/Register/register.html">register</a></h6>
+
+                        </form><!-- End of form -->
+                    </div><!-- End of Card Body-->
+                </div><!-- End of Card -->
+            </div><!-- End of div -->
+        </div><!-- End of Row 1 -->
+    </div><!-- End of Container -->
+
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
