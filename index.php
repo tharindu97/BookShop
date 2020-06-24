e3dc6d0cca9382c387fe82ec518eabce33e8752f
tharindu97 <tharindukavishna@gmail.com>
2020-06-24 11:49:17 +0530

    validation_login

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
                             </div><!-- End of password -->
 
                             <div class="my-4"><!-- button -->
-                                <button class="btn btn-danger btn-block text-uppercase">Login</button>
+                                <button id="btnLogin" class="btn btn-danger btn-block text-uppercase">Login</button>
                             </div><!-- End of button -->
 
                             <hr class="my-4">
@@ -48,5 +48,33 @@
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+
+<script>
+
+    //btnLogin
+    $('#btnLogin').click(
+        function(){
+            inputValidation();
+    });
+    // End of btnLogin
+
+    //inputValidation
+    function inputValidation(callback){
+
+        //Username Validation
+        if($('#txtUserName').val().length === 0){
+            alert('Username can not be empty');
+            return false;
+        }// End of Username Validation
+
+        //Password Validation
+        if($('#txtPassword').val().length === 0){
+            alert('Password can not be empty');
+            return false;
+        }// End of Password Validation
+
+    }// End of inputValidation
+</script>
 </body>
 </html>
